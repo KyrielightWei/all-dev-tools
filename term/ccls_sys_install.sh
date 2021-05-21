a8f76f68ba18aeaf3ca3db3c418a8d8e76492496
pokemonwei <weixx1203@outlook.com>
2021-05-21 08:52:39 +0800

    add nvim,tmux,zsh,ccls install in docker

---
+++ term/ccls_sys_install.sh
@@ -0,0 +1,15 @@
+git clone --depth=1 --recursive https://github.com/MaskRay/ccls
+cd ccls
+
+# Download "Pre-Built Binaries" from https://releases.llvm.org/download.html
+# and unpack to /path/to/clang+llvm-xxx.
+# Do not unpack to a temporary directory, as the clang resource directory is hard-coded
+# into ccls at compile time!
+# See https://github.com/MaskRay/ccls/wiki/FAQ#verify-the-clang-resource-directory-is-correct
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release #-DCMAKE_PREFIX_PATH=/path/to/clang+llvm-xxx
+cmake --build Release
+cmake --build Release --target install
+
+cd ..
+
+rm -rf ccls
--- term/zsh_tmux_sys_init.sh
+++ term/zsh_tmux_sys_init.sh
@@ -1,12 +1,12 @@
 term_dir=$(dirname $(readlink -f "$0"))
 
-git clone https://github.com/zdharma/zinit.git ~/.zinit
-
 ln -s -f "$term_dir/.tmux.conf"  /etc/tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm /root/.tmux/plugins/tpm && /root/.tmux/plugins/tpm/bin/install_plugins
 
-echo "source $term_dir/zshrc.zsh" >> /etc/zshrc
+echo "source $term_dir/zshrc.zsh" >> /etc/zsh/zshrc
+
+git clone https://github.com/zdharma/zinit.git ~/.zinit
 
 zsh
 
